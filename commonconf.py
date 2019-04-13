7da31787038cbbfdc8d4ff6ee4dca4e2a7c649ba
John Mark Schofield <jms@schof.org>
2019-04-12 20:51:33 -0700

    Switching over to python 3.

--- commonconf.py
+++ commonconf.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python
 """Configuration common to all uses of fab."""
 # -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
 from datetime import datetime
 import os.path
 
 AUTHOR = u'John Mark Schofield'
 SITENAME = u'schof.org'
-PATH = 'content'
+SITESUBTITLE = u'A journal of learning and doing'
+PATH = u'content'
 
 STATIC_PATHS = [
-    'wp-content',
-    'media',
-    'non-media-assets',
-    'galleries',
-    'images']
+    u'wp-content',
+    u'media',
+    u'non-media-assets',
+    u'galleries',
+    u'images']
 
-READERS = {'html': None}
+READERS = {u'html': None}
 
-TIMEZONE = 'America/Los_Angeles'
+TIMEZONE = u'America/Los_Angeles'
 DEFAULT_LANG = u'en'
-THEME = 'gum'  # Clean, easy-to-read. Responsive. Good cat & tag support.
+THEME = u'gum'  # Clean, easy-to-read. Responsive. Good cat & tag support.
 
 DEFAULT_PAGINATION = 10
 DEFAULT_ORPHANS = 2
@@ -37,66 +37,66 @@ CURRENT_YEAR = datetime.now().year
 
 DISPLAY_PAGES_ON_MENU = True
 
-PAGE_URL = '{slug}'
-PAGE_SAVE_AS = '{slug}'
+PAGE_URL = u'{slug}'
+PAGE_SAVE_AS = u'{slug}'
 
-TAG_URL = 'tag/{slug}'
-TAG_SAVE_AS = 'tag/{slug}'
+TAG_URL = u'tag/{slug}'
+TAG_SAVE_AS = u'tag/{slug}'
 
-TAGS_SAVE_AS = 'tags'
+TAGS_SAVE_AS = u'tags'
 
-AUTHOR_URL = ''
-AUTHOR_SAVE_AS = ''
-AUTHORS_SAVE_AS = ''
+AUTHOR_URL = u''
+AUTHOR_SAVE_AS = u''
+AUTHORS_SAVE_AS = u''
 
 # AUTHOR_URL = 'author/{slug}'
 # AUTHOR_SAVE_AS = 'author/{slug}'
 # AUTHORS_SAVE_AS = 'authors'
 
-CATEGORY_URL = 'category/{slug}'
-CATEGORY_SAVE_AS = 'category/{slug}'
-CATEGORIES_SAVE_AS = ''
+CATEGORY_URL = u'category/{slug}'
+CATEGORY_SAVE_AS = u'category/{slug}'
+CATEGORIES_SAVE_AS = u''
 
-ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}'
-ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}'
+ARTICLE_URL = u'{date:%Y}/{date:%m}/{date:%d}/{slug}'
+ARTICLE_SAVE_AS = u'{date:%Y}/{date:%m}/{date:%d}/{slug}'
 
-YEAR_ARCHIVE_SAVE_AS = ''
-MONTH_ARCHIVE_SAVE_AS = ''
-ARCHIVES_SAVE_AS = 'archives'
+YEAR_ARCHIVE_SAVE_AS = u''
+MONTH_ARCHIVE_SAVE_AS = u''
+ARCHIVES_SAVE_AS = u'archives'
 
 # YEAR_ARCHIVE_SAVE_AS = '{date:%Y}/all'
 # MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/all'
 # ARCHIVES_SAVE_AS = 'archives'
 
-FEED_DOMAIN = 'https://schof.org/'
+FEED_DOMAIN = u'https://schof.org'
 
-SLUGIFY_SOURCE = "basename"
+SLUGIFY_SOURCE = u"basename"
 
 PLUGIN_PATHS = [
-    os.path.expandvars('$HOME/code/me/pelican-plugins')]
+    os.path.expandvars(u'$HOME/code/others/pelican-plugins')]
 
-PLUGINS = ['sitemap', 'simple_footnotes', 'render_math']
+PLUGINS = [u'sitemap', u'simple_footnotes', u'render_math']
 # GZIP_CACHE_OVERWRITE = False
 
 SITEMAP = {
-    'format': 'xml',
-    'priorities': {
-        'articles': 1,
-        'indexes': 0.5,
-        'pages': 0.5
+    u'format': u'xml',
+    u'priorities': {
+        u'articles': 1,
+        u'indexes': 0.5,
+        u'pages': 0.5
     },
-    'changefreqs': {
-        'articles': 'monthly',
-        'indexes': 'daily',
-        'pages': 'monthly'
+    u'changefreqs': {
+        u'articles': u'monthly',
+        u'indexes': u'daily',
+        u'pages': u'monthly'
     }
 }
 
 
 EXTRA_PATH_METADATA = {
-    'non-media-assets/robots.txt': {'path': 'robots.txt'},
+    u'non-media-assets/robots.txt': {u'path': 'urobots.txt'},
 }
 
 LOAD_CONTENT_CACHE = True
-CHECK_MODIFIED_METHOD = 'mtime'
+CHECK_MODIFIED_METHOD = u'mtime'
 CACHE_CONTENT = True
--- validateconf.py
+++ validateconf.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
-
-# This file is only used if you use `make publish` or
-# explicitly specify it as your config file.
+"""Configuration file for validation and debug."""
 
 import os
 import sys
 sys.path.append(os.curdir)
-from commonconf import *
-from localconf import *
 
-PLUGINS.append('w3c_validate')
+from commonconf import *  # noqa
+from localconf import *  # noqa
+
+# Seems to be broken in python3
+# PLUGINS.append('w3c_validate')
